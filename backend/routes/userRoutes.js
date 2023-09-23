ddffbfaf3ed43ebf0781f8befbdd11627caeed4b
Hashir <hashirshah222@gmail.com>
2023-09-23 01:56:27 -0500

    Added JWT authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser, loginUser} = require('../controlers/userController')
+const {registerUser, loginUser, getMe} = require('../controlers/userController')
+const {protect} = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser )
+router.get('/me', protect, getMe )  // put 'protect' as second argument to use on this sepecific route
 
 // Export with CommonJS syntax
 module.exports = router
